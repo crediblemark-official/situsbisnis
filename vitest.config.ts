c0fd125126c10253edc048cacc94091f373992bd
Rasyiqi <retaslintasbatas@gmail.com>
2026-06-16 11:52:21 +0700

    refactor: memindahkan folder modules ke root proyek (di luar src) dan menyesuaikan konfigurasi alias path

--- vitest.config.ts
+++ vitest.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
+      '@/lib/modules': path.resolve(__dirname, './modules'),
       '@': path.resolve(__dirname, './src'),
     },
   },
